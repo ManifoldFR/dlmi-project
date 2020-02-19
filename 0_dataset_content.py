d3236f58497b83f5dddeb8e5bf6a4c989fecd230
philomenec <philomene.chagniot@gmail.com>
2020-02-19 18:22:59 +0100

    unzip

--- 0_dataset_content.py
+++ 0_dataset_content.py
@@ -1,12 +1,15 @@
-import os
-# import matplotlib.pyplot as plt
-# from PIL import Image
-# import numpy as np
-# import cv2
-# import pandas as pd
 
-DATA_FOLDER = os.path.join("data", "drive", "test", "images")
+
+import os
+import numpy as np
+import cv2
+import pandas as pd
+
+# DATA_FOLDER = os.path.join("data", "drive", "test", "images")
+DATA_FOLDER = os.path.join("data", "stare")
 print(DATA_FOLDER)
+
+diagnose=pd.read_table(os.path.join(DATA_FOLDER,"all-mg-codes.txt"))
 
 
 
