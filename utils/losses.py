2ccd9b3a5a122d3c047ff2129722a882c6521954
ManifoldFR <manifoldfr@outlook.com>
2020-02-24 15:06:46 +0100

    update losses

--- utils/losses.py
+++ utils/losses.py
@@ -7,15 +7,17 @@ EPSILON = 1e-8
 
 
 def iou_pytorch(outputs: torch.Tensor, labels: torch.Tensor) -> torch.Tensor:
-    """
+    """Hard Intersection-over-Union (IoU) metric.
+    
     Source: https://www.kaggle.com/iezepov/fast-iou-scoring-metric-in-pytorch-and-numpy
     """
     # You can comment out this line if you are passing tensors of equal shape
     # But if you are passing output from UNet or something it will most probably
     # be with the BATCH x 1 x H x W shape
     outputs = outputs.squeeze(1)  # BATCH x 1 x H x W => BATCH x H x W
-    intersection = (outputs & labels).float().sum((1, 2))  # Will be zero if Truth=0 or Prediction=0
-    union = (outputs | labels).float().sum((1, 2))         # Will be zero if both are 0
+    dims = (1, 2)  # dimensions to sum over
+    intersection = (outputs & labels).float().sum(dims)  # Will be zero if Truth=0 or Prediction=0
+    union = (outputs | labels).float().sum(dims)         # Will be zero if both are 0
 
     # We smooth our devision to avoid 0/0
     iou = (intersection + EPSILON) / (union + EPSILON)
@@ -47,3 +49,8 @@ def soft_dice_loss(input: torch.Tensor, labels: torch.Tensor, softmax=True) -> t
 def combined_loss(input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
     """CE + Dice"""
     return F.cross_entropy(input, target, reduction="mean") + soft_dice_loss(input, target)
+
+
+def soft_iou_loss(input: torch.Tensor, target: torch.Tensor):
+    """https://arxiv.org/pdf/1705.08790.pdf"""
+    pass
