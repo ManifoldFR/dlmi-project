240a90e93fc99b5356643b67dfcf6ac26355ab29
philomenec <philomene.chagniot@gmail.com>
2020-03-23 17:57:36 +0100

    update dataset content

--- 0_dataset_content.py
+++ 0_dataset_content.py
@@ -5,15 +5,18 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 
+# PROJECT_FOLDER="C:\\Users\\Philo\\Documents\\3A -- MVA\\DL for medical imaging\\retine\\dlmi-project\\"
+
 
 ######################################################################
 ### STARE DATABASE 
 ######################################################################
 
-# PROJECT_FOLDER="C:\\Users\\Philo\\Documents\\3A -- MVA\\DL for medical imaging\\retine\\dlmi-project\\"
 # DATA_FOLDER = os.path.join(PROJECT_FOLDER,"data", "stare")
 DATA_FOLDER = os.path.join("data", "stare")
 print(DATA_FOLDER)
+
+
 diagnose=pd.read_excel(os.path.join(DATA_FOLDER,"diagnose.xls"))
 
 
@@ -55,6 +58,30 @@ for im_name in os.listdir(os.path.join(DATA_FOLDER,"labels","labels_vk")):
     print(im_name)
     im = Image.open(os.path.join(DATA_FOLDER, "labels","labels_vk", im_name))
     im.save(os.path.join(DATA_FOLDER, "annotation 2", str(im_name[:-4]+".png")),"PNG",quality=100)
+
+
+
+
+######################################################################
+### ARIA DATABASE 
+######################################################################
+
+# DATA_FOLDER = os.path.join(PROJECT_FOLDER,"data", "stare")
+DATA_FOLDER = os.path.join("data", "aria")
+
+aria_dict={"images" : os.listdir(os.path.join(DATA_FOLDER, "images")),
+            "STAPLE" : os.listdir(os.path.join(DATA_FOLDER, "STAPLE")),
+            "annot1" : os.listdir(os.path.join(DATA_FOLDER, "annotation 1")),
+            "annot2" : os.listdir(os.path.join(DATA_FOLDER, "annotation 2"))}
+
+aria_dict["annot1"] = [file for file in aria_dict["annot1"] if ".tif" in file]
+aria_dict["annot2"] = [file for file in aria_dict["annot2"] if ".tif" in file]
+aria_dict["healthy"] = [("aria_c_" in file)*1 for file in aria_dict["images"]]
+aria_dict["diabetic"] = [("aria_d_" in file)*1 for file in aria_dict["images"]]
+aria_dict["AMD"] = [("aria_a_" in file)*1 for file in aria_dict["images"]]
+
+aria_df = pd.DataFrame(aria_dict)
+aria_df.to_csv(os.path.join(DATA_FOLDER,"aria_df.csv"))
 
 
 
