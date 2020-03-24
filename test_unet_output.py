de468b84a235fb1e8471dfe979587db1195ed3c3
ManifoldFR <manifoldfr@outlook.com>
2020-03-25 00:42:30 +0100

    test aria, make plot function have max batch size

--- test_unet_output.py
+++ test_unet_output.py
@@ -8,11 +8,14 @@ import torch.nn.functional as F
 def test_unet():
     net = nets.UNet(num_channels=1)
 
-    sets_ = get_datasets('DRIVE')
+    # sets_ = get_datasets('DRIVE')
+    sets_ = get_datasets('ARIA')
     train_dataset = sets_['train']
     train_dataset.return_mask = True  # override
     # tensors, supposedly
-    img, mask, target = zip(*[train_dataset[i] for i in range(2)])
+    # img, mask, target = zip(*[train_dataset[i] for i in range(2)])
+    img, target = zip(*[train_dataset[i] for i in range(2)])
+    mask = None
     import torch
     from torchvision.utils import make_grid
     
@@ -24,7 +27,6 @@ def test_unet():
 
     img = make_grid(img)
     pred_mask = make_grid(pred_mask)
-    mask = make_grid([m.unsqueeze(0) for m in mask])
     target = make_grid([t.unsqueeze(0) for t in target])
     
     import matplotlib.pyplot as plt
@@ -34,8 +36,11 @@ def test_unet():
     img = (std_[1] * img + mean_[1]).clamp(0, 1)
     img = np.moveaxis(img.numpy(), 0, -1)
     
+    if mask is not None:
+        mask = make_grid([m.unsqueeze(0) for m in mask])
+        mask = np.moveaxis(mask.numpy(), 0, -1)
+    
     pred_mask = pred_mask.numpy()[0]
-    mask = np.moveaxis(mask.numpy(), 0, -1)
     target = target.numpy()[0]
     
     fig = plt.figure(figsize=(11, 6))
@@ -50,6 +55,7 @@ def test_unet():
     plt.subplot(2, 2, 3)
     plt.imshow(target)
     plt.axis('off')
+    
     fig.tight_layout()
     plt.show()
 
