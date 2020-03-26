964b9ee55142d698f7ffb12e1c7f8f0998ffa8f4
ManifoldFR <manifoldfr@outlook.com>
2020-03-26 18:10:41 +0100

    adjust dataset statistics for new gamma value

--- compute_dataset_statistics.py
+++ compute_dataset_statistics.py
@@ -9,19 +9,17 @@ import numpy as np
 
 
 
-#SUBSET_SLICE = slice(0, 15)
 # dataset_name = 'DRIVE'
-# train_dataset = DriveDataset("data/drive/training", subset=SUBSET_SLICE)
+# train_dataset = DriveDataset("data/drive/training", subset=DRIVE_SUBSET_TRAIN)
 
-#SUBSET_SLICE = slice(0, 15)
-#dataset_name = 'STARE'
-#SUBSET_SLICE = STARE_SUBSET_TRAIN
-#train_dataset = STAREDataset("data/stare", subset=SUBSET_SLICE)
+dataset_name = 'STARE'
+train_dataset = STAREDataset("data/stare", subset=STARE_SUBSET_TRAIN)
 
-dataset_name = 'ARIA'
-train_dataset = ARIADataset()
+# dataset_name = 'ARIA'
+# train_dataset = ARIADataset()
 
-print(len(train_dataset))
+print("Computing statistics for dataset {:s}".format(dataset_name),
+      len(train_dataset))
 
 #loader = DataLoader(train_dataset, batch_size=8, num_workers=1)
 loader = DataLoader(train_dataset, batch_size=8, num_workers=0)
@@ -44,6 +42,7 @@ std /= len(loader.dataset)
 
 
 import json
+import pprint
 
 try:
     with open("dataset_statistics.json", "r") as f:
@@ -55,7 +54,8 @@ stats_[dataset_name] = {
     'mean': mean.tolist(),
     'std': std.tolist()
 }
-print(stats_)
+
+pprint.pprint(stats_)
 
 with open("dataset_statistics.json", "w") as f:
     json.dump(stats_, f, indent=4)
--- config.py
+++ config.py
@@ -12,8 +12,7 @@ DRIVE_SUBSET_VAL = slice(15, 20)
 STARE_SUBSET_TRAIN = slice(0, 15)
 STARE_SUBSET_VAL = slice(15, 20)
 
-ARIA_SUBSET_TRAIN = slice(0, 107)
-ARIA_SUBSET_VAL = slice(107, 143)
+# no subset slices for ARIA -- see the aria_df.csv for its split
 
 # Input image resolution
 PATCH_SIZE = 320
@@ -21,7 +20,7 @@ PATCH_SIZE = 320
 GAMMA_CORRECTION = 1.7
 
 # model config
-ANTIALIAS = False
+ANTIALIAS = True
 
 MODEL_KWARGS = {
     "antialias": ANTIALIAS,
